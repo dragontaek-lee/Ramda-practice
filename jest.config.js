f7e885bb3766e80800e82a6d36e67abb887c2618
dragontaek-lee <dragontaek98@naver.com>
2024-08-04 03:55:17 +0900

    feat: jest config init

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    moduleFileExtensions: ['ts', 'js'],
     transform: {
-      '^.+\\.(ts|tsx)$': 'ts-jest',
+      '^.+\\.tsx?$': 'ts-jest',
     },
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    transformIgnorePatterns: ['<rootDir>/node_modules/'],
   };
   
